707aca8eb77b18f78abdc43d50139f827fb84530
Abdallah <aabedraba@gmail.com>
2020-10-09 19:28:41 +0200

    save new shortened urls in file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { appendFile } from "fs";
 
 const server = express();
+// server.use(bodyParser.json());
 
 const resources = [{ tag: "business", url: "business.greenmobility.com" }];
 
@@ -8,17 +11,43 @@ server.get("/", (req, res) => {
   const query = req.query;
   const internalUrl = query.url;
   if (internalUrl.includes("go/")) {
+    if (internalUrl === "go/create") {
+      return res.redirect("/create");
+    }
     const resource = internalUrl.split("go/")[1];
     const redirect = resources.find((r) => r.tag === resource);
     if (redirect) {
-      res.redirect("https://" + redirect.url);
+      return res.redirect("https://" + redirect.url);
     } else {
-      res.send("Non existing resource");
+      return res.send("Non existing resource");
     }
   } else {
-    res.redirect(`https://google.com/search?q=${query.url}`);
+    return res.redirect(`https://google.com/search?q=${query.url}`);
   }
 });
+
+server.get("/create", (req, res) => {
+  return res.sendFile(__dirname + "/pages/create/index.html");
+});
+
+server.get("/add", (req, res) => {
+  const query = req.query;
+  if (!query.keyword || !query.url) {
+    return res.send("Wrong parameters");
+  }
+
+  appendFile(
+    "./resources.csv",
+    `${query.keyword}=${query.url}\n`,
+    "utf8",
+    (err) => {
+      if (err) console.log(err);
+    }
+  );
+
+  return res.send("Added");
+});
+
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
